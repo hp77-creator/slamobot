0fb0c25eff8d4fb76974572899d606002c8537ca
Himanshu Pandey <himanshu.dn.pandey@gmail.com>
2025-02-16 18:06:41 +0530

    Fix LLM code

--- src/llm.py
+++ src/llm.py
@@ -6,20 +6,40 @@ class LLM:
     def __init__(self, API_KEY, model_name):
         self.api_key = API_KEY  #change for other models
         self.model_name = model_name
+        self.chat = None
+
+        if 'gemini' in model_name.lower():
+            genai.configure(self.api_key)
+            self.model = genai.GenerativeModel(model_name)
+        else:
+            raise NotImplementedError(f"Model {model_name} is not supported yet")
     
+
     def change_model(self, new_model):
-        if new_model not in genai.list_models:
-            raise Exception("This model is currently not supported")
-        self.model = new_model
-    
-    def get_base_model(self):
-        genai.configure(self.api_key)
-        self.model = genai.GenerativeModel(self.model_name)
-        return self.model 
+        if 'gemini' in new_model.lower():
+            available_models = [model.name for model in genai.list_models()]
+            if new_model not in available_models:
+                raise ValueError(f"Model {new_model} is not available. Available Models: {available_models}")
+            self.model_name = new_model
+            self.model = genai.GenerativeModle(new_model)
+            self.chat = None
+        else:
+            raise NotImplementedError(f"Model {new_model} not added yet.")
     
     def chat_with_history(self, history=[]):
-        self.chat = self.get_base_model().start_chat(history=history)
+        if history is None:
+            history = []
+        if 'gemini' in self.model_name.lower():
+            self.chat = self.model.start_chat(history=history)
+        else:
+            raise NotImplementedError("Chat with history not implemented yet.")
     
     def get_chat_response(self, prompt):
-        self.chat_with_history()
-        return self.chat.send_message(prompt)
+        try:
+            if self.chat is None:
+                self.chat_with_history()
+            
+            response = self.chat.send_message(prompt)
+            return response.text
+        except Exception as e:
+            raise Exception(f"Error getting chat response: {str(e)}")
